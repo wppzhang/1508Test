a7b6a128fd10b7f300fdbfdc838b2d0506678719
qinguang <123@asdf.com>
2015-12-01 10:06:08 +0800

    testtest

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by tarena02_05 on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
